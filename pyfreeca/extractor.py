82516ae3eec7472e0448943a4b64135f768fa00c
Cuesen Tsai <caixuesen@outlook.com>
2020-07-02 01:47:47 +0800

    multiple parts streaming support

--- pyfreeca/extractor.py
+++ pyfreeca/extractor.py
@@ -129,7 +129,10 @@ class AfreecaExtractor(object):
         index = 1
         for m3u8_playlist in m3u8_playlist_list:
             true_m3u8_playlist = self.resolution_confirmation(m3u8_playlist, index)
-            streaming_part_name = video_name + '_' + str(index) + '.mp4'
+            if len(m3u8_playlist) == 1:
+                streaming_part_name = video_name + '.mp4'
+            else:
+                streaming_part_name = video_name + '_' + str(index) + '.mp4'
             index += 1
             streaming_list[streaming_part_name] = true_m3u8_playlist
 
